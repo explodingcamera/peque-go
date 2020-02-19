e08e27d33d27b1cf790f5b5aa55a6e431f94046d
Henry <mail@henrygressmann.de>
2020-02-19 19:44:10 +0100

    feat: add install method for messagedb
    Signed-off-by: Henry <mail@henrygressmann.de>

--- backends/postgres/postgres.go
+++ backends/postgres/postgres.go
@@ -39,6 +39,58 @@ func (s Service) WriteMessage(data string, streamID string) error {
 	return err
 }
 
-func (s Service) Install() {
-	sqlx.LoadFile(s.db, "")
+// Install messageDB on postgres server
+func (s Service) Install() error {
+	prefix := "messagedb/database/"
+	loadFiles := []string{
+		// Main install
+		"roles/message-store.sql",
+		"schema/message-store.sql",
+		"extensions/pgcrypto.sql",
+		"tables/messages.sql",
+
+		// Functions
+		"types/message.sql",
+		"functions/message-store-version.sql",
+		"functions/hash-64.sql",
+		"functions/acquire-lock.sql",
+		"functions/category.sql",
+		"functions/is-category.sql",
+		"functions/id.sql",
+		"functions/cardinal-id.sql",
+		"functions/stream-version.sql",
+		"functions/write-message.sql",
+		"functions/get-stream-messages.sql",
+		"functions/get-category-messages.sql",
+		"functions/get-last-stream-message.sql",
+
+		// Indexes
+		"indexes/messages-id.sql",
+		"indexes/messages-stream.sql",
+		"indexes/messages-category.sql",
+
+		// Privileges
+		"privileges/schema.sql",
+		"privileges/table.sql",
+		"privileges/sequence.sql",
+		"privileges/functions.sql",
+		"privileges/views.sql",
+
+		// Views
+		"views/stream-summary.sql",
+		"views/type-summary.sql",
+		"views/stream-type-summary.sql",
+		"views/type-stream-summary.sql",
+		"views/category-type-summary.sql",
+		"views/type-category-summary.sql",
+	}
+
+	for _, file := range loadFiles {
+		_, err := sqlx.LoadFile(s.db, prefix+file)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
