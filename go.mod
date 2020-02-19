e44f1a10763404ee51e34b40b453140a45f0ecc3
Henry <mail@henrygressmann.de>
2020-02-19 13:05:31 +0100

    chore: add sh interpreter
    Signed-off-by: Henry <mail@henrygressmann.de>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/explodingcamera/peque-go
 
 go 1.13
+
+require mvdan.cc/sh/v3 v3.0.1
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"mvdan.cc/sh/v3/expand"
+	"mvdan.cc/sh/v3/interp"
+)
+
+func main() {
+
+	r, err := interp.New(
+		interp.Dir("./messagedb"),
+		interp.Env(expand.ListEnviron()),
+
+		interp.OpenHandler(func(ctx context.Context, path string, flag int, perm os.FileMode) (io.ReadWriteCloser, error) {
+			if path == "/dev/null" {
+				return nil, nil
+			}
+			return interp.DefaultOpenHandler()(ctx, path, flag, perm)
+		}),
+
+		interp.StdIO(os.Stdin, os.Stdout, os.Stderr),
+	)
+
+	fmt.Println(r, err)
+}
